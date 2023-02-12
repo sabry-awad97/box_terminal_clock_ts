4f38e9aa9f98cb3940e5ef1b54a05d494c266b9e
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-12 09:25:58 +0200

    enhanced version

--- index.ts
+++ index.ts
@@ -1,22 +1,30 @@
 import chalk from 'chalk';
 import boxen from 'boxen';
 
-const clearScreen = () => {
-  process.stdout.write('\x1Bc');
-};
-
 const getTime = (): string => {
   const date = new Date();
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
   const seconds = date.getSeconds().toString().padStart(2, '0');
-  return `${hours}:${minutes}:${seconds}`;
+
+  let time = '';
+  time += chalk.yellowBright(hours + ':');
+  time += chalk.greenBright(minutes + ':');
+  time += chalk.redBright(seconds);
+
+  return time;
 };
 
 const render = () => {
-  clearScreen();
-  // console.log(chalk.greenBright(getTime()));
-  console.log(boxen(getTime()));
+  console.clear();
+  console.log(
+    boxen(getTime(), {
+      padding: 1,
+      margin: 1,
+      borderStyle: 'round',
+      borderColor: 'yellow',
+    })
+  );
 };
 
 setInterval(render, 1000);
